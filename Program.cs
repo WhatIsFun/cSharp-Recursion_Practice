7815166e05a6b2fa8a2b567d1e461af7c55626e4
WhatIsFun <mohammed110051@gmail.com>
2023-07-28 23:42:27 +0400

    Task 1: Fibonacci number

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace cSharp_Recursion_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1: Fibonacci number");
+            Console.WriteLine("Enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fibonacci(n));
+            // Task 1:
+            // Create a recursive function to calculate the nth Fibonacci number. The Fibonacci
+            // sequence is a series of numbers in which each number is the sum of the two
+            // preceding ones: 0, 1, 1, 2, 3, 5, 8, ...
+            static int Fibonacci(int n)
+            {
+                if (n <= 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return Fibonacci(n - 1) + Fibonacci(n - 2);
+                }
+            }
+
+        }
+    }
+}
