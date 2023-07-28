af58536a438797f9b13d1c09b78406c520e16c8c
WhatIsFun <mohammed110051@gmail.com>
2023-07-28 23:56:00 +0400

    Task 2: Factorial Number AND Thask 3: palindrome checker

--- Program.cs
+++ Program.cs
@@ -24,6 +24,40 @@
                 }
             }
 
+            Console.WriteLine("Task 2: Factorial Number");
+            Console.WriteLine("Enter a number:");
+            int f = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(f));
+            // Task 2: Factorial Number
+            // Write a recursive function to calculate the factorial of a given number. The factorial 
+            // of a non - negative integer n is the product of all positive integers less than or equal
+            // to n.
+            static int Factorial(int n)
+            {
+                if (n == 0) { return 1; }
+
+                else { return n * Factorial(n - 1); }
+            }
+
+            Console.WriteLine("Task 3: palindrome checker");
+            Console.WriteLine("Enter a word to check of it is palindrome or not::");
+            string p = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(p));
+            // Task 3: palindrome checker
+            // Write a recursive function to check if a given string is a palindrome.
+            // A palindrome is a word, phrase, number, or other sequence of characters
+            // that reads the same forward and backward.
+            static bool IsPalindrome (string? p) 
+            {
+                if (p.Length <= 1)
+                {
+                    return true;
+                }
+                return p[0] == p[p.Length - 1] && IsPalindrome(p.Substring(1, p.Length - 2));
+            }
+
+
         }
+        
     }
 }
